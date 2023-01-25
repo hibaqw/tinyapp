d8f37f1322ea1b0d20204e79a41614608435a007
Hibaq <hibaqwarsam79@gmail.com>
2023-01-25 06:02:28 +0000

    adding url_new.ejs and posting new urls to database and displaying it functionality

--- express_server.js
+++ express_server.js
@@ -2,12 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+
+function generateRandomString() {
+  const chars = 'ABCEDEFGHIGKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charsLength = chars.length;
+  for (let i = 0; i < 6; i++){
+    result += chars.charAt(Math.floor(Math.random()* charsLength));
+  }
+  return result; 
+  
+}
 app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -24,7 +35,15 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  let id = generateRandomString();
+  urlDatabase[id]= req.body.longURL;
+  res.redirect(`/urls/${id}`);
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]/* What goes here? */ };
   res.render("urls_show", templateVars);
