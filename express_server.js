d5d4ce88e83daf6dcddfcd3cd49cb707ec60b3c9
Hibaq <hibaqwarsam79@gmail.com>
2023-01-26 06:12:44 +0000

    added updating url functionality

--- express_server.js
+++ express_server.js
@@ -52,6 +52,10 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id]= req.body.longURL;
+  res.redirect('/urls');
+});
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
 
