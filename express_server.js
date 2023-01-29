089f9e575886e9ca17d43648309128c0f84d64c8
Hibaq <hibaqwarsam79@gmail.com>
2023-01-29 07:15:47 +0000

    fixed bug inside getEmail function

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 
-
+//database of users
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -16,6 +16,13 @@ const users = {
     password: "dishwasher-funk",
   },
 };
+//database of urls 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+//function that generates random 6 letter alphanumeric strings
+//for user_id url database
 function generateRandomString() {
   const chars = 'ABCEDEFGHIGKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -24,19 +31,27 @@ function generateRandomString() {
     result += chars.charAt(Math.floor(Math.random()* charsLength));
   }
   return result; 
-  
 }
 
 function lookUpUser(user_id){
   users[user_id]? users[user_id]: undefined;
 }
+
+function getUserEmail(email){
+
+  for (const key in users){
+    if (users[key][email]) {
+        return email;
+    }
+  }
+  return undefined;
+
+}
+//middleware
 app.set("view engine", "ejs");
 app.use(cookieParser())
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -49,27 +64,33 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// display short and long urls on /urls page
 app.get("/urls", (req, res) => {
   const user = lookUpUser(req.cookies["user_id"]);
   const templateVars = { urls: urlDatabase, user: user};
   res.render("urls_index", templateVars);
 });
+
+// display new short url on /urls/new page"
 app.get("/urls/new", (req, res) => {
   // const templateVars = { username: req.cookies["username"]}
   const user = lookUpUser(req.cookies["user_id"]);
   const templateVars = {user: user};
   res.render("urls_new", templateVars);
 });
+//display new long and short url on urls page
 app.post("/urls", (req, res) => {
-  // console.log(req.body); // Log the POST request body to the console
   let id = generateRandomString();
   urlDatabase[id]= req.body.longURL;
   res.redirect(`/urls/${id}`);
 });
+
+//delete specfic url and redirect back to /urls page
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
+//display clickable short url on /urls/id page
 app.get("/urls/:id", (req, res) => {
   const user = lookUpUser(req.cookies["user_id"]);
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: user};
@@ -79,39 +100,56 @@ app.post("/urls/:id", (req, res) => {
   urlDatabase[req.params.id]= req.body.longURL;
   res.redirect('/urls');
 });
+
+// redirect from /u/id to long url by clicking clickable short link
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
-
+//if longURL doesn't exist
+//send status 404 response
   if(!longURL){
-    res.render("urls_error");
+    res.sendStatus(404);
   }
+  //else redirect to longURL
   else{
     res.redirect(longURL);
   }
 });
-
-app.post("/login", (req,res) => {
+// get login info and render login page
+app.get("/login", (req,res) => {
   // res.cookie('user_id',lookUpUser(req.cookie["user_id"]));
-  res.redirect('/urls');
+  const user = lookUpUser(req.cookies["user_id"]);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: user};
+  res.render('urls_login',templateVars);
 
 });
-
+// handle login response
 app.post("/logout", (req,res) => {
   res.clearCookie('user_id');
   res.redirect('/urls');
 });
+
+// render login page
 app.get("/register", (req,res) =>{
   //const user = users["userRandomID"];
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: undefined};
   res.render("urls_register", templateVars);
 })
+// handle login response
 app.post("/register", (req,res) =>{
-  const user_id  = generateRandomString();
-  users[user_id] = {id : user_id, email: req.body.email, password: req.body.password};
-  console.log("USER_ID",users[user_id]);
-  res.cookie("user_id", user_id);
-  console.log(users);
-  res.redirect('/urls');
+  // const user_email = req.body.email;
+  // const user_pass = req.body.password;
+  if(req.body.email === "" || req.body.password === "" || getUserEmail(req.body.email)){
+    console.log(getUserEmail(req.body.email));
+    res.sendStatus(404);
+  }
+  else{
+    const user_id  = generateRandomString();
+    users[user_id] = {id : user_id, email: req.body.email, password: req.body.password};
+    console.log("USER_ID",users[user_id]);
+    res.cookie("user_id", user_id);
+    console.log(users);
+    res.redirect('/urls');
+  }
 })
 
 app.listen(PORT, () => {
